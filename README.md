376ea2707b91e985844b8ccbd627db8b510dde2c
MarKco <ilsecondodasinistra@gmail.com>
2017-11-18 12:27:17 +0100

    Changed gitignore

--- README.md
+++
@@ -1,4 +0,0 @@
-parakeet
-========
-
-Parakeet is a clever alarm clock: wakes you up when it knows you have time to do what you have to.
---
+++ app/src/main/java/com/ilsecondodasinistra/parakeet/MyApplication.java
@@ -0,0 +1,12 @@
+package com.ilsecondodasinistra.parakeet;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// The following line triggers the initialization of ACRA
+	}
+}
