9c4d4b5ffca8628e742a884e7ff780b23aecf89a
MarKco <ilsecondodasinistra@gmail.com>
2017-11-18 16:30:21 +0100

    Before instant apping

--- app/src/main/java/com/ilsecondodasinistra/parakeet/DrawerLayoutHelper.java
+++ app/src/main/java/com/ilsecondodasinistra/parakeet/DrawerLayoutHelper.java
@@ -1,7 +1,5 @@
 package com.ilsecondodasinistra.parakeet;
 
-import settings.ParakeetSettingsActivity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -13,6 +11,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import settings.ParakeetSettingsActivity;
 
 public class DrawerLayoutHelper {
 
