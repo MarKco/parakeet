254937e24e8bb116009656d2962439fe45745e4a
marco.zanetti <marco.zanetti@metide.com>
2017-11-20 18:29:25 +0100

    New release

--- parakeetshared/src/main/java/com/ilsecondodasinistra/parakeet/shared/AboutActivity.java
+++ parakeetshared/src/main/java/com/ilsecondodasinistra/parakeet/shared/AboutActivity.java
@@ -1,6 +1,8 @@
 package com.ilsecondodasinistra.parakeet.shared;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
@@ -11,16 +13,20 @@ public class AboutActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
-		
-		TextView emailTextView = (TextView)findViewById(R.id.authorEmailTV);
+
+		TextView emailTextView = (TextView) findViewById(R.id.authorEmailTV);
 		emailTextView.setMovementMethod(LinkMovementMethod.getInstance());
-		
-		TextView appInfoTextView = (TextView)findViewById(R.id.appInfoTV);
+
+		TextView appInfoTextView = (TextView) findViewById(R.id.appInfoTV);
 		appInfoTextView.setMovementMethod(LinkMovementMethod.getInstance());
-		
-		TextView appAuthorTextView = (TextView)findViewById(R.id.appAuthorTV);
-		appAuthorTextView.setMovementMethod(LinkMovementMethod.getInstance());		
-		
+
+		TextView appAuthorTextView = (TextView) findViewById(R.id.appAuthorTV);
+		appAuthorTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+		// ATTENTION: This was auto-generated to handle app links.
+		Intent appLinkIntent = getIntent();
+		String appLinkAction = appLinkIntent.getAction();
+		Uri appLinkData = appLinkIntent.getData();
 	}
 
 }
